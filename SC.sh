b68c0b8e7b746506d64e25f46d1c19615caaacc0
Nymouse-X <93504283+Nymouse-X@users.noreply.github.com>
2022-06-30 22:08:28 +0700

    Add files via upload

---
+++ SC.sh
@@ -0,0 +1,541 @@
+echo "1"
+clear
+echo "2"
+clear
+echo "3"
+clear
+echo "4"
+clear
+echo "5"
+clear
+echo "6"
+clear
+echo "7"
+clear
+echo "8"
+clear
+echo "9"
+clear
+echo "10"
+clear
+echo "11"
+clear
+echo "12"
+echo "Memperoses code"  
+sleep 0.1
+clear
+echo "mEmperoses code"  
+sleep 0.1
+clear
+echo "meMperoses code"  
+sleep 0.1
+clear
+echo "memPeroses code" 
+sleep 0.1
+clear
+echo "mempEroses code"  
+sleep 0.1
+clear
+echo "mempeRoses code"  
+sleep 0.1
+clear
+echo "memperOses code" 
+sleep 0.1
+clear
+echo "memperoSes code"  
+sleep 0.1
+clear
+echo "memperosEs code"  
+sleep 0.1
+clear
+echo "memperoseS code"  
+sleep 0.1
+clear
+echo "memperoses Code"  
+sleep 0.1
+clear
+echo "memperoses cOde"  
+sleep 0.1
+clear
+echo "memperoses coDe"  
+sleep 0.1
+clear
+echo "memperoses codE"  
+sleep 0.1
+clear
+echo "Memperoses code" 
+sleep 0.1
+clear
+echo "mEmperoses code" 
+sleep 0.1
+clear
+echo "meMperoses code" 
+sleep 0.1
+clear
+echo "memPeroses code" 
+sleep 0.1
+clear
+echo "mempEroses code" 
+sleep 0.1
+clear
+echo "mempeRoses code" 
+sleep 0.1
+clear
+echo "memperOses code" 
+sleep 0.1
+clear
+echo "memperoSes code" 
+sleep 0.1
+clear
+echo "memperosEs code" 
+sleep 0.1
+clear
+echo "memperoseS code" 
+sleep 0.1
+clear
+echo "memperoses Code" 
+sleep 0.1
+clear
+echo "memperoses cOde" 
+sleep 0.1
+clear
+echo "memperoses coDe" 
+sleep 0.1
+clear
+echo "memperoses codE" 
+sleep 0.1
+clear
+echo "Memperoses code"  
+sleep 0.1
+clear
+echo "mEmperoses code"  
+sleep 0.1
+clear
+echo "meMperoses code"  
+sleep 0.1
+clear
+echo "memPeroses code"  
+sleep 0.1
+clear
+echo "mempEroses code"  
+sleep 0.1
+clear
+echo "mempeRoses code"  
+sleep 0.1
+clear
+echo "memperOses code"  
+sleep 0.1
+clear
+echo "memperoSes code" 
+sleep 0.1
+clear
+echo "memperosEs code" 
+sleep 0.1
+clear
+echo "memperoseS code"  
+sleep 0.1
+clear
+echo "memperoses Code" 
+sleep 0.1
+clear
+echo "memperoses cOde" 
+sleep 0.1
+clear
+echo "memperoses coDe"  
+sleep 0.1
+clear
+echo "memperoses codE" 
+sleep 0.1
+clear
+echo "Memperoses code"  
+sleep 0.1
+clear
+echo "mEmperoses code" 
+sleep 0.1
+clear
+echo "meMperoses code"  
+sleep 0.1
+clear
+echo "memPeroses code" 
+sleep 0.1
+clear
+echo "mempEroses code"  
+sleep 0.1
+clear
+echo "mempeRoses code"  
+sleep 0.1
+clear
+echo "memperOses code" 
+sleep 0.1
+clear
+echo "memperoSes code"  
+sleep 0.1
+clear
+echo "memperosEs code"  
+sleep 0.1
+clear
+echo "memperoseS code"  
+sleep 0.1
+clear
+echo "memperoses Code" 
+sleep 0.1
+clear
+echo "memperoses cOde"  
+sleep 0.1
+clear 
+echo "memperoses coDe" 
+sleep 0.1
+clear
+echo "memperoses codE"  
+sleep 0.1
+clear
+figlet SpamChat-X | lolcat 
+date | lolcat
+echo "Selamat Datang Di SpamChat ver.X By NymouseX" | lolcat 
+echo "_______________________________________________" | lolcat 
+echo 
+echo "Silakan Isi Julukan Anda :"
+read -p "[-}-->" NM
+clear
+sleep 1
+echo "S"
+sleep 0.1
+clear
+echo "sE"
+sleep 0.1
+clear
+echo "seD"
+sleep 0.1
+clear
+echo "sedA"
+sleep 0.1
+clear
+echo "sedaN"
+sleep 0.1
+clear
+echo "sedanG"
+sleep 0.1
+clear
+echo "sedang M"
+sleep 0.1
+clear
+echo "sedang mE"
+sleep 0.1
+clear
+echo "sedang meM"
+sleep 0.1
+clear
+echo "sedang memP"
+sleep 0.1
+clear
+echo "sedang mempR"
+sleep 0.1
+clear
+echo "sedang memprO"
+sleep 0.1
+clear
+echo "sedang memproS"
+sleep 0.1
+clear
+echo "sedang memprosE"
+sleep 0.1
+clear
+echo "sedang memeproseS"
+sleep 0.1
+clear
+echo "sedang memproses J"
+sleep 0.1
+clear
+echo "sedang memproses jU"
+sleep 0.1
+clear
+echo "sedang memproses juL"
+sleep 0.1
+clear
+echo "sedang memproses julU"
+sleep 0.1
+clear
+echo "sedang memeproses juluK"
+sleep 0.1
+clear
+echo "sedang memeproses julukA"
+sleep 0.1
+clear
+echo "sedang memeproses julukaN"
+sleep 0.1
+clear
+echo "sedang memeproses julukan."
+sleep 0.1
+clear
+echo "sedang memeproses julukan.."
+sleep 0.1
+clear
+echo "sedang memeproses julukan..."
+sleep 0.1
+clear
+figlet SpamChat-X | lolcat
+echo "Salam Tuan $NM " | lolcat
+date | lolcat
+echo "_______________________________________________" | lolcat 
+echo 
+echo "Mohon Masukan Nomor Target :"
+read -p "[$NM}-->" NO
+clear
+sleep 1
+echo "S"
+sleep 0.1
+clear
+echo "sE"
+sleep 0.1
+clear
+echo "seD"
+sleep 0.1
+clear
+echo "sedA"
+sleep 0.1
+clear
+echo "sedaN"
+sleep 0.1
+clear
+echo "sedanG"
+sleep 0.1
+clear
+echo "sedang M"
+sleep 0.1
+clear
+echo "sedang mE"
+sleep 0.1
+clear
+echo "sedang meM"
+sleep 0.1
+clear
+echo "sedang memP"
+sleep 0.1
+clear
+echo "sedang mempR"
+sleep 0.1
+clear
+echo "sedang memprO"
+sleep 0.1
+clear
+echo "sedang memproS"
+sleep 0.1
+clear
+echo "sedang memprosE"
+sleep 0.1
+clear
+echo "sedang memeproseS"
+sleep 0.1
+clear
+echo "sedang memeproses N"
+sleep 0.1
+clear
+echo "sedang memeproses nO"
+sleep 0.1
+clear
+echo "sedang memeproses noM"
+sleep 0.1
+clear
+echo "sedang memeproses nomO"
+sleep 0.1
+clear
+echo "sedang memeproses nomoR"
+sleep 0.1
+clear
+echo "sedang memeproses nomor T"
+sleep 0.1
+clear
+echo "sedang memeproses nomor tA"
+sleep 0.1
+clear
+echo "sedang memeproses nomor taR"
+sleep 0.1
+clear
+echo "sedang memeproses nomor tarG"
+sleep 0.1
+clear
+echo "sedang memeproses nomor targE"
+sleep 0.1
+clear
+echo "sedang memeproses nomor targeT"
+sleep 0.1
+clear
+echo "sedang memeproses nomor target."
+sleep 0.1
+clear
+echo "sedang memeproses nomor target.."
+sleep 0.1
+clear
+echo "sedang memeproses nomor target..."
+sleep 0.1
+clear
+figlet SpamChat-X | lolcat
+echo "Salam Tuan $NM " | lolcat
+date | lolcat
+echo "_______________________________________________" | lolcat 
+echo 
+echo "Mohon Masukan Kata Yang Ingin Kamu Sampaikan :"
+read -p "[$NM}-->" SPY
+echo "Mohon Masukan Jumlah Spam (1-50)"
+read -p "[$NM}-->" JWB
+echo "Memulai Proses"
+echo "[>                              <]"
+sleep 0.1
+clear
+echo "[>>                            <<]"
+sleep 0.1
+clear
+echo "[>>>                          <<<]"
+sleep 0.1
+clear
+echo "[>>>>                        <<<<]"
+sleep 0.1
+clear
+echo "[>>>>>                      <<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>                    <<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>                  <<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>                <<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>              <<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>>            <<<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>>>          <<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>>>>        <<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>>>>>      <<<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>>>>>>    <<<<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>>>>>>>^^<<<<<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>>>>>>>^^<<<<<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>>>>>>    <<<<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>>>>>      <<<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>>>>        <<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>>>          <<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>>            <<<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>              <<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>                <<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>                  <<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>                    <<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>                      <<<<<]"
+sleep 0.1
+clear
+echo "[>>>>                        <<<<]"
+sleep 0.1
+clear
+echo "[>>>                          <<<]"
+sleep 0.1
+clear
+echo "[>>                            <<]"
+sleep 0.1
+clear
+echo "[>                              <]"
+sleep 0.1
+clear
+echo "[>                              <]"
+sleep 0.1
+clear
+echo "[>>                            <<]"
+sleep 0.1
+clear
+echo "[>>>                          <<<]"
+sleep 0.1
+clear
+echo "[>>>>                        <<<<]"
+sleep 0.1
+clear
+echo "[>>>>>                      <<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>                    <<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>                  <<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>                <<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>              <<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>>            <<<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>>>          <<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>>>>        <<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>>>>>      <<<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>>>>>>    <<<<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>>>>>>>^^<<<<<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "[>>>>>>>>>>>>>>>^^<<<<<<<<<<<<<<<]"
+sleep 0.1
+clear
+figlet SpamChat-X | lolcat
+echo "Salam Tuan $NM " | lolcat
+date | lolcat
+echo "_______________________________________________" | lolcat 
+echo 
+echo "Detail"
+echo "Nomor : $NO "
+echo "Pesan : $SPY "
+echo "Jumlah : $JWB "
+echo "_______________________________________________" | lolcat
+echo
+sleep 2
+figlet SUKSES.. | lolcat
+
+
+
+
+
+
+
+
+
